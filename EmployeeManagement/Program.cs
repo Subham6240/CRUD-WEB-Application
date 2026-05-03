40e83bdc7714e0623d285940f7a11895cabb9662
Subham6240 <70630080+Subham6240@users.noreply.github.com>
2026-05-04 00:05:40 +0530

    Update Program.cs

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,1 +1,46 @@
+using EmployeeManagement.Data;
+using EmployeeManagement.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;    
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseInMemoryDatabase("EmployeeDb")
+    );
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCors", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+//Add the employee repository to the dependency injection(DI)
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseCors("MyCors");
+
+app.MapControllers();
+
+app.Run();
 
